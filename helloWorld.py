9afbdd58f5d135df23b829d09f32ccdc20576e55
TomBruso <71333295+TomBruso@users.noreply.github.com>
2021-03-25 12:46:52 -0400

    Added Facebook login

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,11 @@
 print ("Hello World")
 print ("Hello World, my name is Thomas Bruso and I love Python!")
 userName = input ("Hello! Please enter your name!")
-print ("Hello", userName, "nice to meet you!")
-
-
+print ("Hello", userName, "nice to meet you")
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+if(userName.lower() == "thomas bruso" and userPassword == "football"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
 
