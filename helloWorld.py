ba2a241698cb8e0cdd00faf93ebf5f6a6935e2a6
TomBruso <71333295+TomBruso@users.noreply.github.com>
2021-03-25 11:49:52 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 print ("Hello World")
 print ("Hello World, my name is Thomas Bruso and I love Python!")
 userName = input ("Hello! Please enter your name!")
-print ("Hello", userName, "nice to meet you")
+print ("Hello", userName, "nice to meet you!")
 
 
 
